35248a98517649ed6048dd8e496a5f7a5262bf77
irshad vali <irshadvali@irshads-MacBook-Pro.local>
2023-09-11 13:00:28 +0530

    build file

--- home/src/Counter.jsx
+++ home/src/Counter.jsx
@@ -1,10 +1,24 @@
 import React , {useState}from "react";
-export default function Counter (){
+export default function Counter ({titleOne='INC', titleTwo='DEC', chValue}){
     const [count, setCount] = useState(0);
+
+  const myInc = ()=>{
+      setCount(count + 1);
+      chValue && chValue(count + 1)
+  }  
+  const myDec = ()=>{
+    setCount(count - 1);
+    chValue && chValue(count - 1)
+}
     return (
         <div className=" p-5 bg-green-50 text-black text-3xl font-bc">
             <p>{`conunt is ${count}`}</p>
-            <button onClick={() => setCount(count + 1)}>Increment</button>
+            <div>
+            <button onClick={() => myInc()}>{titleOne}</button>
+            </div>
+            <div>
+            <button onClick={() => myDec()}>{titleTwo}</button>
+            </div>
         </div>
     )
 }
--- pdp/src/App.jsx
+++ pdp/src/App.jsx
@@ -1,16 +1,24 @@
-import React from "react";
+import React, { Suspense, useState } from "react";
 import ReactDOM from "react-dom";
 
 import "./index.scss";
 import Header from "home/Header";
 import Footer from "home/Footer";
 import Counter from "home/Counter";
-const App = () => (
-  <div className="text-3xl mx-auto max-w-6xl">
-    <Header></Header>
-    <div>This is my PDP paget</div>
-    <Counter></Counter>
-    <Footer></Footer>
-  </div>
-);
+const App = () => {
+const [current, setCurrent]=useState(0)
+  const chValue = (data)=>{
+    setCurrent(data)
+}  
+
+  return(<div style={{margin: '10px'}}>
+  <Header></Header>
+  <div>This is my PDP paget </div>
+  <div>{current}</div>
+  <Counter titleOne={'Increment'} chValue={chValue} titleTwo={'Decrement'}></Counter>
+  
+  <Footer></Footer>
+</div>);
+  
+};
 ReactDOM.render(<App />, document.getElementById("app"));
